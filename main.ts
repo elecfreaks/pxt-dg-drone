e067d1dfcb63381065eb36c78e622f7f1879a776
Llane <Lion_yan@outlook.com>
2023-03-02 17:51:19 +0800

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,24 @@
-basic.forever(function () {
-	
-})
+/**
+* Functions to PlanetX sensor by Llane.
+*/
+//% color=#56B7F7  icon="\uf005" block="XG171-DRONE" blockId="XG171"
+namespace PlanetX_Basic {
+
+    /**
+    * TODO: full speed move back,speed is -100.
+    */
+    //% blockId=cutebot_back block="Reverse at full speed"
+    //% weight=85
+    export function backforward(): void {
+        // Add code here
+        let buf = pins.createBuffer(4);
+        buf[0] = 0x01;
+        buf[1] = 0x01;
+        buf[2] = 80;
+        buf[3] = 0;
+        pins.i2cWriteBuffer(0x20, buf);
+        buf[0] = 0x02;
+        pins.i2cWriteBuffer(0x20, buf);
+
+    }
+}
