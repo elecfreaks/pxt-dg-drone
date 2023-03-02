251a3dc3bf6ae9ea17659a66731b7d9210fd9829
Llane <Lion_yan@outlook.com>
2023-03-02 18:04:59 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -2,23 +2,25 @@
 * Functions to PlanetX sensor by Llane.
 */
 //% color=#56B7F7  icon="\uf005" block="XG171-DRONE" blockId="XG171"
-namespace PlanetX_Basic {
+namespace XG171_DRONE {
+
 
     /**
-    * TODO: full speed move back,speed is -100.
+    * TODO: Set horizontal flight speed.
     */
-    //% blockId=cutebot_back block="Reverse at full speed"
-    //% weight=85
-    export function backforward(): void {
+    //% block="Set horizontal flight speed %speed cm/s"
+    //% speed.min=0 speed.max=100
+    //% weight=90
+    export function set_horizontal_speed(speed: number): void {
         // Add code here
-        let buf = pins.createBuffer(4);
-        buf[0] = 0x01;
-        buf[1] = 0x01;
-        buf[2] = 80;
-        buf[3] = 0;
-        pins.i2cWriteBuffer(0x20, buf);
-        buf[0] = 0x02;
-        pins.i2cWriteBuffer(0x20, buf);
-
+    }
+    /**
+    * TODO: Set vertical flight speed.
+    */
+    //% block="Set vertical flight speed %speed cm/s"
+    //% speed.min=0 speed.max=100
+    //% weight=89
+    export function set_vertical_speed(speed: number): void {
+        // Add code here
     }
 }
